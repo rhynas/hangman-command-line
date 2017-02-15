458db2f7f478112e48ba9e40671e716346b81a88
Rhynas <rhynas@gmail.com>
2017-02-14 22:18:28 -0500

    correct word checkup

--- game.js
+++ game.js
@@ -1,13 +1,58 @@
+// Load the NPM Package inquirer
 var inquirer = require('inquirer')
+
+// Load the word Constructor
 var Word = require('./Word.js');
 
-var words = ['jeff', 'john', 'rhyna'];
+//Array with words by theme
+var musicalInstruments = ['guitar', 'piano', 'violin', 'saxophone', 'trumpet', 'cello', 'accordion', 'clarinet', 'harmonica', 'trombone'];
+var musicGenres = ['jazz', 'blues', 'country', 'reggae', 'electronic', 'alternative', 'classical', 'gospel', 'reggaeton', 'hardcore', 'Salsa'];
+var olimpicSports = ['archery', 'badminton', 'basketball', 'boxing', 'cycling', 'football', 'diving', 'fencing', 'gymnastics', 'marathon', 'swimming', 'weightlifting', 'volleyball', 'triathlon', 'wrestling'];
+var fruits = ['Banana', 'grapefruit', 'mandarin', 'watermelon', 'strawberry', 'cantaloupe', 'dragonfruit', 'coconut', 'cranberry', 'pineapple', 'pomegranite', 'papaya'];
 
-var wordToPlay = words[Math.floor(Math.random()*words.length)];
+var wordToPlay = "";
+var wordObject;
+var guessCount = 0;
+var maxGuesses = 6;
 
-var wordObject = new Word(wordToPlay);
-wordObject.makeAndPushLettersIntoWord();
-console.log(wordObject.display());
+//Prompt to the user the themes options for selection
+function selectTheme(){
+    inquirer.prompt([
+    {
+    type: "list",
+    name: "theme",
+    message: "Select a Theme: ",
+     // choices: ["Bulbasaur", "Squirtle", "Charmander"],
+   // new inquirer.Separator()
+   choices:["Musical Instruments", "Music Genres", 
+            "Olimpic Sports", "Fruits", 
+            ]
+    },
+    ]).then(function(data){
+        console.log(data.theme);
+        switch(data.theme) {
+            case "Musical Instruments":
+                wordToPlay = musicalInstruments[Math.floor(Math.random()*musicalInstruments.length)];
+                break;
+            case "Music Genres":
+                wordToPlay = musicGenres[Math.floor(Math.random()*musicGenres.length)];
+                 break;
+            case "Olimpic Sports":
+                wordToPlay = olimpicSports[Math.floor(Math.random()*olimpicSports.length)];
+                 break;
+            default:
+                wordToPlay = fruits[Math.floor(Math.random()*fruits.length)];
+                 break;
+        }
+
+        wordObject = new Word(wordToPlay);
+        console.log(wordToPlay);
+        wordObject.init();
+        console.log(wordObject.display());
+        askLetter();
+
+    });
+}
 
 function askLetter(){
     inquirer.prompt([
@@ -16,14 +61,32 @@ function askLetter(){
     name: "guess",
     message: "What letter do you guess? If you are done then say no."},
     ]).then(function(data){
-        if (data.guess != 'no') {
-            wordObject.updateLetter(data.guess);
+        // if (data.guess != 'no') {
+        if(!wordObject.correctWord()){
+            wordObject.updateLetter(data.guess.toLowerCase());
 
             console.log(wordObject.display());
 
             askLetter();
         }
+        else{
+
+            inquirer.prompt([
+            {
+                type: "list",
+                name: "playAgain",
+                message: "Would you like to Play again? ",
+                choices:["yes", "no"]
+            },
+            ]).then(function(play){
+                if (play.playAgain == "yes") selectTheme();
+            });
+            // guessCount++;
+            // if(guessCount == maxGuesses){
+            //     console.log('You run out of Guesses');
+            // }
+        }
     });
 }
 
-askLetter();
+selectTheme();
--- word.js
+++ word.js
@@ -3,7 +3,7 @@ var Letter = require('./Letter.js');
 function Word(word){
     this.word = word;
     this.letters = [];
-    this.makeAndPushLettersIntoWord = function(){
+    this.init = function(){
         for (var i=0; i< this.word.length; i++){
             var lett = new Letter(this.word[i]);
             this.letters.push(lett);
@@ -12,23 +12,34 @@ function Word(word){
     this.display = function(){
         var str = "";
         for (var i=0; i < this.letters.length; i++){
-            str = str + this.letters[i].display();
+            str = str + " " + this.letters[i].display();
         }
 
         return str;
-    }
+    },
     this.updateLetter = function(guess){
-        //check all of the letter objects and see if the guess matches
-        //if it does I update that letter's found to true
-
-        //another way
-        // var index = this.word.indexOf(guess);
-        // if (index > -1) this.letters[index].found = true;
-
         //one way
-        for (var i=0; i<this.letters.length; i++){
-            if (this.letters[i].letter == guess) this.letters[i].found = true;
-        }
+        // for (var i=0; i<this.letters.length; i++){
+        //     if (this.letters[i].letter == guess){
+        //     	this.letters[i].found = true;
+        //     	return(true);
+        //     }
+        //     return false;
+        // }
+		for (var i = 0; i < this.letters.length; i++) {
+				if (this.letters[i].letter == guess){
+					this.letters[i].found = true;
+				}
+			}
+		
+    }
+    this.correctWord = function(){
+    	for (var i = 0; i < this.letters.length; i++) {
+    		if (this.letters[i].found === false) {
+    			return false;
+    		}
+    	}
+    	return true;
     }
 }
 
